20f4c7e76a6db0f4073d7632e9da49d34055e29c
jwest <jwest@jwest.pl>
2012-10-06 23:52:58 +0200

    remove fseek (bugs?)

--- lib/GhBlog/JsonRequestParser.php
+++ lib/GhBlog/JsonRequestParser.php
@@ -5,46 +5,26 @@ namespace GhBlog;
 class JsonRequestParser {
 
 	protected $_stream;
-	protected $_streamSize;
 
-	public function __construct($stream = null, $size = 0) {
-		if ($stream === null) {
-			$this->_stream = $this->_getStream();
-			$this->_streamSize = $this->_getStreamSize();
-			return;
-		}
-		$this->_stream = $stream;
-		$this->_streamSize = $size;
+	public function __construct($stream = null) {
+		$this->_stream = ($stream === null)
+			? $this->_getDefaultStream()
+			: $stream;
 	}
 
-	protected function _getStream() {
-		return fopen ('php://input', 'r');
+	protected function _getDefaultStream() {
+		return fopen('php://input', 'r');
 	}
 
-	protected function _getStreamSize() {
+	protected function _getDefaultStreamSize() {
 		return (int)$_SERVER['CONTENT_LENGTH'];
 	}
 
-	public function getRealStreamSize() {
-		$stream = tmpfile ();
-	    $realSize = stream_copy_to_stream($this->_stream, $stream);
-	    fclose ($stream);
-	    fseek($this->_stream, 0, SEEK_SET);
-	    return $realSize;
-	}
-
 	public function read() {
-		$this->_validationSize($this->getRealStreamSize());
 		$obj = json_decode(stream_get_contents($this->_stream));
 		if ($obj === null)
 			throw new \Exception('Content is invalid');
-		fseek($this->_stream, 0, SEEK_SET);
 		return $obj;
-	}
-
-	protected function _validationSize($realSize) {
-		if ($realSize !== $this->_streamSize)
-			throw new \Exception('Stream length is invalid!');
 	}
 
 }
